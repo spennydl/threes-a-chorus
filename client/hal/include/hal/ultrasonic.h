10616567f9e4a918f0d70b5188caec97eb841cb1
Louie <louie_lu@sfu.ca>
2024-02-23 10:03:00 -0800

    Change trigger pulse from GPIO to PWM; address PR comments

--- client/hal/include/hal/ultrasonic.h
+++ client/hal/include/hal/ultrasonic.h
@@ -5,10 +5,10 @@
  */
 
 // TODO:
-// 1) Change Trigger to PWM
 // 2) Implement hysteresis to avoid inaccurate, large swings in readings.
 
 #include "hal/gpio.h"
+#include "hal/pwm.h"
 #include "hal/timeutils.h"
 
 #include <assert.h>
@@ -18,21 +18,23 @@
 #pragma once
 
 /**
- * Configure the BeagleBone, then create a thread to start
- * the ultrasonic sensor.
+ * Configure the BeagleBone's PWM and GPIO for the ultrasonic sensor.
+ * PWM is used by the trigger to emit a pulse for 10us every 200ms.
+ * GPIO is used by the echo to receive the trigger pulse.
  * @return bool Whether or not the init succeeded.
  */
 bool
 Ultrasonic_init(void);
 
 /**
- * Shutdown the ultrasonic sensor thread.
+ * Shutdown the ultrasonic sensor.
  */
 void
 Ultrasonic_shutdown(void);
 
 /**
  * Get the distance reading from the ultrasonic sensor in cm.
+ * @return long double Distance to the nearest object in cm.
  */
 long double
 Ultrasonic_getDistanceInCm(void);
