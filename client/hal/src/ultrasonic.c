16b645b798899ae6faa694d9d43eb2acd27164f9
Louie <louie_lu@sfu.ca>
2024-02-23 10:03:00 -0800

    Change trigger pulse from GPIO to PWM; address PR comments

--- client/hal/src/ultrasonic.c
+++ client/hal/src/ultrasonic.c
@@ -1,15 +1,10 @@
 #include "hal/ultrasonic.h"
 
-#define PULSE_FREQUENCY_IN_MS 200
-#define PULSE_WIDTH_IN_US 0.01
-#define SPEED_OF_SOUND_IN_NS 343.0 / 1e9
+#define DUTY_CYCLE_10US 0.00005
+#define NS_TO_CM_CONVERSION 0.000017150
 
 // Local variables
-static gpio triggerPin = { .linux_pin_number = 48,
-                           .bbg_header = 9,
-                           .bbg_pin_number = 15,
-                           .direction = GPIO_OUT,
-                           .__gpio = NULL }; // TODO: __gpio = NULL correct?
+static pwm triggerChannel = { .channel = PWM_CHANNEL0B, .__pwm = NULL };
 static gpio echoPin = { .linux_pin_number = 49,
                         .bbg_header = 9,
                         .bbg_pin_number = 23,
@@ -17,46 +12,19 @@ static gpio echoPin = { .linux_pin_number = 49,
                         .__gpio = NULL };
 
 static bool initialized = false;
-static pthread_t ultrasonic_t;
-
-// Prototype
-static void*
-Ultrasonic_emitPulse(void* args);
-
-/**
- * Emit a trigger pulse from the ultrasonic sensor
- * every 200ms, each with a duration of 0.01ms (10us).
- */
-static void*
-Ultrasonic_emitPulse(void* args)
-{
-    assert(initialized);
-    (void)args;
-
-    while (1) {
-        Timeutils_sleepForMs(PULSE_FREQUENCY_IN_MS);
-        gpio_write(&triggerPin, 1);
-        Timeutils_sleepForMs(PULSE_WIDTH_IN_US);
-        gpio_write(&triggerPin, 0);
-    }
-}
 
 bool
 Ultrasonic_init(void)
 {
-    gpio_open(&triggerPin);
     gpio_open(&echoPin);
     gpio_set_active_low(&echoPin, false);
 
-    initialized = true;
+    pwm_open(&triggerChannel);
+    pwm_set_duty_cycle(&triggerChannel, DUTY_CYCLE_10US);
+    pwm_set_period(&triggerChannel, 5);
+    pwm_enable(&triggerChannel, true);
 
-    // Create thread
-    int rc;
-    if ((rc = pthread_create(
-           &ultrasonic_t, NULL, Ultrasonic_emitPulse, NULL)) != 0) {
-        perror("pthread_create:");
-        initialized = false; // TODO: thoughts on this init fail handling?
-    }
+    initialized = true;
 
     return initialized;
 }
@@ -66,8 +34,9 @@ Ultrasonic_shutdown(void)
 {
     assert(initialized);
 
-    pthread_cancel(ultrasonic_t);
-    pthread_join(ultrasonic_t, NULL);
+    gpio_close(&echoPin);
+    pwm_close(&triggerChannel);
+
     initialized = false;
 }
 
@@ -79,6 +48,7 @@ Ultrasonic_getDistanceInCm(void)
     long double startTimeInNs;
     long double endTimeInNs;
     long double elapsedTimeInNs;
+
     int echoValue;
 
     // echoValue automatically changes from 0 to 1
@@ -94,20 +64,18 @@ Ultrasonic_getDistanceInCm(void)
         endTimeInNs = Timeutils_getTimeInNs();
     } while (echoValue == 1);
 
+    // TODO: Tried gpio_wait(). The readings didn't work well for me but I might
+    // be missing something.
+    // gpio gpioPins[1] = { echoPin };
+    // int outVals[1];
+
+    // startTimeInNs = Timeutils_getTimeInNs();
+    // int eventsReceived = gpio_wait(gpioPins, 1, outVals, 1000);
+    // endTimeInNs = Timeutils_getTimeInNs();
+
     elapsedTimeInNs = endTimeInNs - startTimeInNs;
 
-    // Calculations (3):
-    // distance = speed * time; halve the distance; convert m -> cm
-    long double totalDistanceInM = SPEED_OF_SOUND_IN_NS * elapsedTimeInNs;
-    long double distanceOfOneTripInM = totalDistanceInM / 2;
-    long double distanceInCm = distanceOfOneTripInM * 100;
-
-    // TODO: Could also do this at the expense of a magic number.
-    // long double distanceInCm = elapsedTimeInNs * 0.000017150;
-
-    // if (distanceInCm <= 0) {
-    //     distanceInCm = 0;
-    // }
+    long double distanceInCm = elapsedTimeInNs * NS_TO_CM_CONVERSION;
 
     return distanceInCm;
 }
