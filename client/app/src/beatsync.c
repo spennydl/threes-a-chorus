c725a2baad3a53cc7976c41fb076fd9554c22bdc
Spencer Leslie <spencerdleslie@gmail.com>
2024-03-18 13:06:10 -0700

    move stuff around to try to sync better

--- client/app/src/beatsync.c
+++ client/app/src/beatsync.c
@@ -1,13 +1,13 @@
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <unistd.h>
 
-#include "hal/timeutils.h"
-#include "hal/tcp.h"
 #include "beatsync.h"
+#include "hal/tcp.h"
+#include "hal/timeutils.h"
 #include "midiPlayer.h"
 
 static bool running = false;
@@ -33,7 +33,7 @@ void
 BeatSync_requestBeatOffsetAndMidi()
 {
     Tcp_requestFile("test.midi");
-    char beatOffsetString[MAX_BUFFER_SIZE] = {0};
+    char beatOffsetString[MAX_BUFFER_SIZE] = { 0 };
     Tcp_makeServerRequest(OFFSET_CODE, beatOffsetString);
     MidiPlayer_setBeatOffset(strtoll(beatOffsetString, NULL, 10));
 }
@@ -42,23 +42,27 @@ void*
 beatRequesterWorker(void* p)
 {
     (void)p;
-    char res[MAX_BUFFER_SIZE] = {0};
+    char res[MAX_BUFFER_SIZE] = { 0 };
     long msToSleep;
     long msBetweenBeats;
 
-    while(running) {
+    while (running) {
+        // printf("Slept for %ldms but now ...Beat!\n", msToSleep);
+        MidiPlayer_playNextBeat();
+
+        long long reqTimeStart = Timeutils_getTimeInNs();
         memset(res, 0, MAX_BUFFER_SIZE);
         Tcp_makeServerRequest(BEAT_CODE, res);
+        long long reqTime = Timeutils_getTimeInNs() - reqTimeStart;
 
         msToSleep = strtol(strtok(res, ";"), NULL, 10);
         msBetweenBeats = strtol(strtok(NULL, ";"), NULL, 10);
+        if (reqTime > 0) {
+            msToSleep -= reqTime;
+        }
 
         MidiPlayer_setBpm(60000 / msBetweenBeats);
         Timeutils_sleepForMs(msToSleep);
-        
-        //printf("Slept for %ldms but now ...Beat!\n", msToSleep);
-        MidiPlayer_playNextBeat();
-        
     }
 
     return NULL;
