37dd86ea096bd22cb4de3f963223e24938db6466
Louie <112336312+louieluuu@users.noreply.github.com>
2024-04-08 11:21:10 -0700

    Add segDisplay eyes, config file for Zen Cape Red and Green (#25)

---
+++ client/common/include/com/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdlib.h>
+
+int
+Utils_getRandomIntBtwn(int min, int max);
---
+++ client/common/src/utils.c
@@ -0,0 +1,17 @@
+#include "com/utils.h"
+
+int
+Utils_getRandomIntBtwn(int min, int max)
+{
+    // Reference:
+    // https://stackoverflow.com/questions/822323/how-to-generate-a-random-int-in-c
+    // Used only as a starting point.
+
+    // Example shown with max = 3000, min = 500
+    int range = max - min;                   // 2500
+    int rangeInclusive = range + 1;          // 2501
+    int randomInt = rand() % rangeInclusive; // 0    -> 2500
+    int randomIntInRange = randomInt + min;  // 500  -> 3000
+
+    return randomIntInRange;
+}
